2f2572f08b235d04540993fa96e96c2a0b64cd5a
learner-prakharrr <126413965+learner-prakharrr@users.noreply.github.com>
2023-10-12 01:04:20 +0530

    Update bfs.cpp
    make it easy ,shorter and add some comments for better understanding

--- bfs.cpp
+++ bfs.cpp
@@ -1,72 +1,60 @@
-// BFS algorithm in C++
-
 #include <iostream>
-#include <list>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
 class Graph {
-  int numVertices;
-  list<int>* adjLists;
-  bool* visited;
+    int numVertices;
+    vector<vector<int>> adjList;
 
-   public:
-  Graph(int vertices);
-  void addEdge(int src, int dest);
-  void BFS(int startVertex);
+public:
+    Graph(int vertices);
+    void addEdge(int src, int dest);
+    void BFS(int startVertex);
 };
 
-// Create a graph with given vertices,
-// and maintain an adjacency list
 Graph::Graph(int vertices) {
-  numVertices = vertices;
-  adjLists = new list<int>[vertices];
+    numVertices = vertices;
+    adjList.resize(vertices);
 }
 
-// Add edges to the graph
 void Graph::addEdge(int src, int dest) {
-  adjLists[src].push_back(dest);
-  adjLists[dest].push_back(src);
+    adjList[src].push_back(dest);
+    adjList[dest].push_back(src);
 }
 
-// BFS algorithm
 void Graph::BFS(int startVertex) {
-  visited = new bool[numVertices];
-  for (int i = 0; i < numVertices; i++)
-    visited[i] = false;
+    vector<bool> visited(numVertices, false);
+    queue<int> q;
 
-  list<int> queue;
+    visited[startVertex] = true;
+    q.push(startVertex);
 
-  visited[startVertex] = true;
-  queue.push_back(startVertex);
+    while (!q.empty()) {
+        int currVertex = q.front();
+        cout << "Visited " << currVertex << " ";
+        q.pop();
 
-  list<int>::iterator i;
-
-  while (!queue.empty()) {
-    int currVertex = queue.front();
-    cout << "Visited " << currVertex << " ";
-    queue.pop_front();
-
-    for (i = adjLists[currVertex].begin(); i != adjLists[currVertex].end(); ++i) {
-      int adjVertex = *i;
-      if (!visited[adjVertex]) {
-        visited[adjVertex] = true;
-        queue.push_back(adjVertex);
-      }
+        for (int adjVertex : adjList[currVertex]) {
+            if (!visited[adjVertex]) {
+                visited[adjVertex] = true;
+                q.push(adjVertex);
+            }
+        }
     }
-  }
 }
 
 int main() {
-  Graph g(4);
-  g.addEdge(0, 1);
-  g.addEdge(0, 2);
-  g.addEdge(1, 2);
-  g.addEdge(2, 0);
-  g.addEdge(2, 3);
-  g.addEdge(3, 3);
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
 
-  g.BFS(2);
+    g.BFS(2);
 
-  return 0;
+    return 0;
 }
